7729dd44c9bd276f8cacf4ea28eabc4f0f85eaec
ogatodesuyo <tenten_flog@icloud.com>
2020-04-20 22:17:52 +0900

    使用する関数ファイルの作成、初期設定

---
+++ function.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+from linebot.exceptions import (
+    LineBotApiError, InvalidSignatureError
+)
+import logging
