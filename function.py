03492d352a2d571af89042d2c618faa0ceddba37
ogatodesuyo <tenten_flog@icloud.com>
2020-04-20 22:18:37 +0900

    LINEbotのトークン設定

--- function.py
+++ function.py
@@ -10,3 +10,18 @@ from linebot.exceptions import (
     LineBotApiError, InvalidSignatureError
 )
 import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    logger.error('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
